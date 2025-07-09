7825b3d259515b138b55fd40758b2c537d89ed1f
Juan Camilo Guerrero Ortega <55815692+juancamiloguerrero@users.noreply.github.com>
2025-07-09 10:33:19 -0500

    conexion con backend para el despliegue del forntend

--- formulario/src/app/components/formulario.js
+++ formulario/src/app/components/formulario.js
@@ -268,7 +268,7 @@ export default function Formulario() {
         vendedores
       };
 
-      fetch("http://localhost:8000/generar-doc/", {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/generar-doc/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
